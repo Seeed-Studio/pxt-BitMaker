85822bd950d06d7a97c8df9498eba9f445dfe1a4
MiroChao <531414952@qq.com>
2019-01-29 20:03:45 +0800

    Updates.

--- main.ts
+++ main.ts
@@ -14,9 +14,23 @@ namespace sample {
     /**
      * Render a boolean as a high/low toggle
      */
-    //% block="123$high|    123123"
+    //% block="digital pin $pin| is %high"
+    //% pin.fieldEditor="gridpicker"
+    //% pin.fieldOptions.width=60
+    //% pin.fieldOptions.columns=1
+    //% pin.fieldOptions.tooltips="false"
     //% high.shadow="toggleHighLow"
-    export function highLow(high: boolean): boolean {
-        return high;
+    export function read_Din(pin: available_pins, high: boolean): boolean {
+        let Din_stat: number;
+        if (pin == available_pins.P0) {
+            Din_stat = pins.digitalReadPin(DigitalPin.P0);
+            if (high == true && Din_stat == 1) {
+                return toggle;
+            }
+            toggle = pins.digitalReadPin(DigitalPin.P0);
+        } else if (condition) {
+
+        }
+        return toggle;
     }
 }
