7f9662ad77d058f6af858d5c8f250efd11c4b371
MiroChao <531414952@qq.com>
2019-01-29 17:00:14 +0800

    Updates.

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
-enum  GPIOs {
+enum GPIOs {
     //% block="P0"
-    P0, 
+    P0,
     //% block="P1"
-    P1, 
+    P1,
 
 }
 /**
  * Provides access to BitMaker micro:bit functionality.
  */
-
+//% color=190 icon="\uf126" block="BitMaker"
 namespace sample {
 
     //% block
